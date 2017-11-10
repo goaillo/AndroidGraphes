c87ede5efab53ede56bbc08a5e220452e8ed31cc
goaillo <gaelrebours@gmail.com>
2017-11-10 10:36:09 +0100

    Ajout d'un debut de fonction pour afficher une fleche

--- app/src/main/java/classes/DrawableGraph.java
+++ app/src/main/java/classes/DrawableGraph.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.PixelFormat;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.graphics.drawable.Drawable;
 import android.media.midi.MidiInputPort;
 import android.util.Log;
@@ -26,6 +27,97 @@ public class DrawableGraph extends Drawable {
         graph = g;
     }
 
+    public Path findPoint(Path edgePath, RectF r)
+    {
+        float[] interieur = {0f, 0f};
+        float[] ext = {0f, 0f};
+        float[] finalext = {0f, 0f};
+        float mid = 0.5f;
+        int i = 0;
+        float x;
+        float y;
+        int v = 0;
+        boolean b = true;
+        do
+        {
+            Path path = new Path();
+            path.addRoundRect(r, 40, 40, Path.Direction.CW);
+            path.computeBounds(r, true);
+            Region region = new Region();
+            region.setPath(path, new Region());
+            PathMeasure pm = new PathMeasure(edgePath, false);
+            float[] point = {0f, 0f};
+            pm.getPosTan(pm.getLength() * mid, point, null);
+            boolean d = r.contains(point[0], point[1]);
+            Log.v("test", "result d :" + d);
+            if(d)
+            {
+                interieur[0] = point[0];
+                interieur[1] = point[1];
+                mid = mid + mid + (mid/(2));
+            }
+            else
+            {
+                ext[0] = point[0];
+                ext[1] = point[1];
+                mid = mid / (2);
+            }
+            i++;
+            double distance = Math.sqrt(Math.pow((interieur[0]-ext[0]), 2) + Math.pow((interieur[1]-ext[1]), 2));
+            if(distance <= 60 && v ==0)
+            {
+                Log.v("c bon", "on est bon");
+                finalext[0] = ext[0];
+                finalext[1] = ext[1];
+                Node testNode2 = new Node(ext[0],ext[1],"",Color.GREEN);
+                testNode2.setRayonDefault(10);
+                graph.addNode(testNode2);
+                v = 1;
+            }
+        }while(i != 30);
+        //Fait 30 fois la recherche dichotomique, prend le premier point trouvé en dehors du noeud des qu'il y'a une distance minimale de 60
+        //et le point intérieur est le dernier trouvé dans la recherche dichotomique
+        Node testNode = new Node(interieur[0],interieur[1],"",Color.YELLOW);
+        testNode.setRayonDefault(10);
+        graph.addNode(testNode);
+
+        //Calcul du vecteur directeur afin d'avoir les autres points
+        float deltaX = interieur[0] - finalext[0];
+        float deltaY = interieur[1] - finalext[1];
+        float frac = (float) 0.1;
+
+
+        float point_x_1 = finalext[0] + (float) ((1 - frac) * deltaX + frac * deltaY);
+        float point_y_1 = finalext[1] + (float) ((1 - frac) * deltaY - frac * deltaX);
+
+        float point_x_2 = interieur[0];
+        float point_y_2 = interieur[1];
+
+        float point_x_3 = finalext[0] + (float) ((1 - frac) * deltaX - frac * deltaY);
+        float point_y_3 = finalext[1] + (float) ((1 - frac) * deltaY + frac * deltaX);
+
+        Node pointb = new Node(point_x_3,point_y_3,"",Color.RED);
+        pointb.setRayonDefault(10);
+        graph.addNode(pointb);
+
+        Node pointc = new Node(point_x_3,point_y_3,"",Color.RED);
+        pointc.setRayonDefault(10);
+        graph.addNode(pointc);
+
+        Path pfleche = new Path();
+        pfleche.setFillType(Path.FillType.EVEN_ODD);
+
+        //Cree un path créant la fleche
+        pfleche.moveTo(finalext[0], finalext[1]);
+        pfleche.lineTo(point_x_1, point_y_1);
+        pfleche.lineTo(point_x_2, point_y_2);
+        pfleche.lineTo(point_x_3, point_y_3);
+        pfleche.lineTo(finalext[0], finalext[1]);
+        pfleche.close();
+
+        return pfleche;
+    }
+
     @Override
     public void draw(Canvas canvas) {
 
@@ -35,6 +127,9 @@ public class DrawableGraph extends Drawable {
         pArc.setStyle(Paint.Style.STROKE);
         Path path;
         Path pathTemp;
+
+
+
         //On dessine d'abord les arcs
         for(ArcFinal a : graph.getArcs()){
             float [] midPoint = {0f, 0f};
